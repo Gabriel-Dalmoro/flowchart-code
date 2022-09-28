08fd53fdfd42915955ad2b20a6e1324440d8ea3e
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-09-28 15:52:44 -0600

    added a shoe randomizer into seperate branch

--- onlyRunOnce.js
+++ onlyRunOnce.js
@@ -1,5 +1,11 @@
 const {checkTires} = require("./checkTires");
 
+const getRandomShoe = (shoeArray) => {
+    const length = shoeArray.length;
+    const randomIndex = Math.floor(Math.random()*length);
+    return shoeArray[randomIndex];
+}
+myShoes = ["nikes", "addidas", "New Balances"];
 
 const onlyRunOnce = (haveRunAlready, areTiresInflated) => {
     if (haveRunAlready) {
@@ -7,7 +13,7 @@ const onlyRunOnce = (haveRunAlready, areTiresInflated) => {
         checkTires(areTiresInflated);
     } else {
         console.log("Have I run already this week?", haveRunAlready);
-        console.log("Put on running shoes");
+        console.log(`Put on ${getRandomShoe(myShoes)} shoes`);
         console.log("Run!");
     }
 };
